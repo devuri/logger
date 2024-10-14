27ec6bf15aa6d2d72fb0e3af81cde5da4dde831f
devuri <devuri@users.noreply.github.com>
2024-10-13 23:22:27 -0500

    inti

---
+++ src/Log.php
@@ -0,0 +1,148 @@
+<?php
+
+namespace Urisoft;
+
+use Psr\Log\LoggerInterface;
+
+class Log
+{
+    private static ?LoggerInterface $logger = null;
+
+    /**
+     * Initializes the logger with an optional file path. If no file path is provided,
+     * it will fall back to error_log.
+     *
+     * @param null|LoggerInterface $logger
+     */
+    public static function init(?LoggerInterface $logger = null): void
+    {
+        self::$logger = $logger;
+    }
+
+    /**
+     * Log an emergency message.
+     *
+     * @param string $message
+     * @param array $context
+     */
+    public static function emergency(string $message, array $context = []): void
+    {
+        self::getLogger()->emergency($message, $context);
+    }
+
+    /**
+     * Log an alert message.
+     *
+     * @param string $message
+     * @param array $context
+     */
+    public static function alert(string $message, array $context = []): void
+    {
+        self::getLogger()->alert($message, $context);
+    }
+
+    /**
+     * Log a critical message.
+     *
+     * @param string $message
+     * @param array $context
+     */
+    public static function critical(string $message, array $context = []): void
+    {
+        self::getLogger()->critical($message, $context);
+    }
+
+    /**
+     * Log an error message.
+     *
+     * @param string $message
+     * @param array $context
+     */
+    public static function error(string $message, array $context = []): void
+    {
+        self::getLogger()->error($message, $context);
+    }
+
+    /**
+     * Log a warning message.
+     *
+     * @param string $message
+     * @param array $context
+     */
+    public static function warning(string $message, array $context = []): void
+    {
+        self::getLogger()->warning($message, $context);
+    }
+
+    /**
+     * Log a notice message.
+     *
+     * @param string $message
+     * @param array $context
+     */
+    public static function notice(string $message, array $context = []): void
+    {
+        self::getLogger()->notice($message, $context);
+    }
+
+    /**
+     * Log an informational message.
+     *
+     * @param string $message
+     * @param array $context
+     */
+    public static function info(string $message, array $context = []): void
+    {
+        self::getLogger()->info($message, $context);
+    }
+
+    /**
+     * Log a debug message.
+     *
+     * @param string $message
+     * @param array $context
+     */
+    public static function debug(string $message, array $context = []): void
+    {
+        self::getLogger()->debug($message, $context);
+    }
+
+    /**
+     * Ensure that the logger is initialized and return the logger instance.
+     *
+     * @return LoggerInterface
+     */
+    private static function getLogger(): LoggerInterface
+    {
+        return self::$logger;
+    }
+
+	/**
+	 * Ensure that a log file exists. If the file or its parent directories do not exist,
+	 * this function will create them.
+	 *
+	 * @param string $logFile The path to the log file.
+	 * @return void
+	 * @throws RuntimeException If the file cannot be created.
+	 */
+	public static function createLogFile(string $logFile): void
+	{
+	    $logDir = dirname($logFile);
+
+	    if (!is_dir($logDir)) {
+	        if (!mkdir($logDir, 0777, true) && !is_dir($logDir)) {
+	            throw new RuntimeException("Unable to create directory: $logDir");
+	        }
+	    }
+
+	    if (!file_exists($logFile)) {
+	        if (file_put_contents($logFile, '') === false) {
+	            throw new RuntimeException("Unable to create log file: $logFile");
+	        }
+	    }
+
+	    if (!is_writable($logFile)) {
+	        throw new RuntimeException("Log file is not writable: $logFile");
+	    }
+	}
+}
